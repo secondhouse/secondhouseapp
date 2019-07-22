5e25d64cb6a53895ca0158f29a67517d97fb33b3
secondhouse <53023962+secondhouse@users.noreply.github.com>
2019-07-22 09:49:16 +0900

    Update 20190602004516_add_dog_choice_to_request.rb

--- db/migrate/20190602004516_add_dog_choice_to_request.rb
+++ db/migrate/20190602004516_add_dog_choice_to_request.rb
@@ -1,5 +1,5 @@
-class AddDogChoiceToRequest < ActiveRecord::Migration[5.2]
-  def change
-    add_column :requests, :dog_choice, :string
-  end
-end
+#class AddDogChoiceToRequest < ActiveRecord::Migration[5.2]
+ # def change
+  #  add_column :requests, :dog_choice, :string
+  #end
+#end
