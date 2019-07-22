89ac094e7e544e4c1c3e90a1d635abf3a22df80b
secondhouse <53023962+secondhouse@users.noreply.github.com>
2019-07-23 08:28:31 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,41 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+rails_root = File.expand_path('../../', __FILE__)
+
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+worker_processes 2
+
+working_directory rails_root
+
+timeout 30
+
+stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+
+###注
+# Nginxで使用する場合は以下の設定を行う。
+# listen File.expand_path('/var/run/unicorn/unicorn.sock', __FILE__)
+# Unicorn単体で使う場合は以下。
+listen 8080
+
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+
 preload_app true
 
-listen '/Users/shinkaihideto/Desktop/secondhouse_test0719:2/tmp/unicorn.sock'
-pid    '/Users/shinkaihideto/Desktop/secondhouse_test0719:2/tmp/unicorn.pid'
-
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
-
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
